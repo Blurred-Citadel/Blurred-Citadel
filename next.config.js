81b46c0f6587bcb4e07cdd6c89344e21b4681360
Blurred-Citadel <ashtonchurch@ymail.com>
2024-11-06 22:42:45 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
+  distDir: '.next',
   images: {
     unoptimized: true,
   },
+  // Remove output: 'export' if it's there
 }
 
 module.exports = nextConfig
