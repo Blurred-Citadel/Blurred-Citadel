1a27b0a16a92e17beb1775f19eb04419bdc132f1
Blurred-Citadel <ashtonchurch@ymail.com>
2024-11-06 22:05:15 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,9 @@
+import MonitoringDashboard from '@/components/monitoring-dashboard'
 
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <MonitoringDashboard />
+    </main>
+  )
+}
