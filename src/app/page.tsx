a9b63e63a0f69e8a948e24328ec4e626fd16f93a
Blurred-Citadel <ashtonchurch@ymail.com>
2024-11-07 08:11:03 +0000

    Delete src/app directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MonitoringDashboard from '@/components/monitoring-dashboard'
-
-export default function Home() {
-  return (
-    <main className="container mx-auto p-4">
-      <MonitoringDashboard />
-    </main>
-  )
-}
--- src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div className="p-4">
-      <h1>Test Page</h1>
-      <p>If you can see this, routing is working!</p>
-    </div>
-  )
-}
